0aa0d0a315e83eb8e0bffed3d85f321eae45494f
bilabila <bilabila@qq.com>
2024-08-27 01:32:16 +0800

    move libload call out

--- host/src/main.rs
+++ host/src/main.rs
@@ -7,14 +7,15 @@ fn my_func() -> u32 {
     0
 }
 
-fn main() {
-    fn call_dynamic() -> Result<u32, Box<dyn std::error::Error>> {
-        unsafe {
-            let lib = libloading::Library::new("target/debug/libguest.so")?;
-            let func: libloading::Symbol<unsafe extern "C" fn() -> u32> = lib.get(b"my_func")?;
-            Ok(func())
-        }
+fn call_dynamic() -> Result<u32, Box<dyn std::error::Error>> {
+    unsafe {
+        let lib = libloading::Library::new("target/debug/libguest.so")?;
+        let func: libloading::Symbol<unsafe extern "C" fn() -> u32> = lib.get(b"my_func")?;
+        Ok(func())
     }
+}
+
+fn main() {
     for i in 0.. {
         // leak
         let out = call_dynamic();
